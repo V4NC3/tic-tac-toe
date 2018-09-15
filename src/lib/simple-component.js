eb069373af67aa47b9fb984df60e6d902af9aef4
V4NC3 <nguyen.vance@gmail.com>
2018-09-14 20:21:00 -0700

    added delete element func

---
+++ src/lib/simple-component.js
@@ -0,0 +1,15 @@
+export class SimpleComponent {
+    /* This class generates a element property containing a NodeElement to be appended into the DOM. This class should be the base of every other component we will create and therefore should also contain its removal from DOM. @param selector {String} the selector of the element to be created. */
+
+    constructor( selector ){
+        if (!selector)
+            throw Error("a SimpleComponent must be composed of a selector");
+        this.selector = selector.toString();
+        this.element = document.createElement(this.selector);
+    }
+
+    /* Call this when you want to remove the element from the DOM*/
+    destroy(){
+        document.body.removeChild(this.element);
+    }
+}
