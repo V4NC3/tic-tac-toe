a310672bc7a92e5cc17c6547988bcebe4073b7a7
V4NC3 <nguyen.vance@gmail.com>
2018-09-14 23:54:26 -0700

    completed getter and setter for textContent

--- src/lib/writable-component.js
+++ src/lib/writable-component.js
@@ -1,1 +1,18 @@
-import { simpleComponent } from "./simple-component";
+import { SimpleComponent } from "./simple-component";
+
+/* A writable class holds an alias property to change the elements' textContent and extends SimpleComponent */
+export class WritableComponent extends SimpleComponent {
+  constructor(selector) {
+    super(selector);
+  }
+
+  /* Get proprety function */
+  get textContent() {
+    return this.element.textContent;
+  }
+
+  /* Setter proprety function; v = 'some value' */
+  set textContent(v) {
+    return (this.element.textContent = v);
+  }
+}
