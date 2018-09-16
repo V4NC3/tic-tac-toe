fdaef0d59b8cb60eedebf7b42d443bf57210ac48
V4NC3 <nguyen.vance@gmail.com>
2018-09-16 15:59:05 -0700

    completed symbol in play

--- src/components/play-symbol.js
+++ src/components/play-symbol.js
@@ -1,3 +1,13 @@
 import { WritableComponent } from "../lib/writable-component";
 
 /* Shows the Symbol that's in play */
+export class PlaySymbol extends WritableComponent {
+  constructor() {
+    super("symbol");
+    this.element.setAttribute("style", "float: right");
+  }
+
+  set textContent(v) {
+    super.textContent = `playing: ${v}`;
+  }
+}
