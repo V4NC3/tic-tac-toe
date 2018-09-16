a65ea5350696149ba6d86de949756533df93fe93
V4NC3 <nguyen.vance@gmail.com>
2018-09-16 00:15:49 -0700

    init writing components, created GameSlot Class that connects with the GameEngine with a setting and overwriting the base Class to display game updates

--- src/components/game-slot.js
+++ src/components/game-slot.js
@@ -1,1 +1,28 @@
 import { WritableComponent } from "../lib/writable-component";
+
+/* GameSlot Class that's maintaining the visual correlation between the GameEngine */
+export class GameSlot extends WritableComponent {
+  /* Class takes slot as an argument */
+
+  constructor(slot) {
+    super("game-slot");
+
+    /* Style element after calling the Super Class. Calling all the browser functionalities */
+    this.element.setAttribute(
+      "style",
+      "height: 60px; width: 60px; background-color: grey; display: inline-block; " +
+        "border: 1px solid black; margin: 5px; font-size: large; color: black; line-height: 60px;" +
+        "text-align: center; cursor: pointer"
+    );
+
+    this.element.setAttribute("slot-row", slot.row);
+    this.element.setAttribute("slot-column", slot.column);
+    this.element.textContent = "-";
+  }
+
+  /* Overwriting the base Class because we want to do more than just setting the value */
+  set textContent(slot) {
+    super.textContent = slot.symbol;
+    this.element.style.backgroundColor = "white";
+  }
+}
