1df88b90fb52434ba5aa239872050487f9c19e6b
V4NC3 <nguyen.vance@gmail.com>
2018-09-16 16:42:14 -0700

    init turninformation which connects to the currentTurn and symbol component

---
+++ src/components/turn-information.js
@@ -0,0 +1,16 @@
+import { SimpleComponent } from "../lib/simple-component";
+import { PlaySymbol } from "./play-symbol";
+import { CurrentTurn } from "./current-turn";
+
+/* Wraps CurrentTurn and Symbol into a whole Component */
+export class TurnInformation extends SimpleComponent {
+  constructor() {
+    super("turn-information");
+    this.currentTurn = new CurrentTurn();
+    this.symbol = new PlaySymbol();
+    this.element.setAttribute(
+      "style",
+      "text-transform: uppercase; font-size: 30px; height: 40px; display: block; font-family: Monospace"
+    );
+  }
+}
