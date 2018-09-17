fb41064adc4a0a4bf166a9ad2669c630ba95fbc6
V4NC3 <nguyen.vance@gmail.com>
2018-09-16 17:35:30 -0700

    init state of inital component condition when the game start

--- src/modules/game-hud.js
+++ src/modules/game-hud.js
@@ -4,3 +4,14 @@ import { Notice } from "../components/notice";
 import { TurnInformation } from "../components/turn-information";
 
 /* GameHud Class brings all the Components together into the DOM and the hub between the user interface and the Game Engine */
+export class GameHud {
+  constructor() {
+    this.turns = null;
+    this.gameEngine = null;
+    this.gameField = null;
+    this.turnInfo = new TurnInformation();
+
+    document.body.appendChild(this.turnInfo.element);
+    this.createGameField(false);
+  }
+}
