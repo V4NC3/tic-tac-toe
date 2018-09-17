13de19f6fbe5af40fd8da95a86bb45f576c8a0f8
V4NC3 <nguyen.vance@gmail.com>
2018-09-16 17:26:49 -0700

    init gamehub to join all the components together and bring the UI and Game Engine together

---
+++ src/modules/game-hud.js
@@ -0,0 +1,6 @@
+import { GameEngine } from "../lib/game-engine";
+import { GameField } from "../components/game-field";
+import { Notice } from "../components/notice";
+import { TurnInformation } from "../components/turn-information";
+
+/* GameHud Class brings all the Components together into the DOM and the hub between the user interface and the Game Engine */
